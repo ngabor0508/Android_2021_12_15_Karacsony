09a9a5f7a1544c3c1032db8d1a438e2a76c4a079
ngabor0508 <ngabor0508@gmail.com>
2021-12-15 16:44:17 +0100

    Elso.

---
+++ app/src/main/java/com/nagy_gabor_2021_12_15_karacsony/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nagy_gabor_2021_12_15_karacsony;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
